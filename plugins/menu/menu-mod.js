06bad2c02724ff92954f21b224fe6bf5ef486524
rileydomina333 <indoxxabile666@gmail.com>
2026-05-09 12:12:23 +0200

    Update menu-mod.js

--- plugins/menu/menu-mod.js
+++ plugins/menu/menu-mod.js
@@ -9,7 +9,7 @@ const handler = async (message, { conn, usedPrefix, command }) => {
     const groupId = message.isGroup ? message.chat : null;
 
     const menuText = generateMenuText(usedPrefix, userId, groupId);
-    const imagePath = path.join(__dirname, '../../media/IMG-20260501-WA0549.jpg');
+    const imagePath = path.join(__dirname, '../../media/IMG-20260504-WA0069.jpg');
 
     await conn.sendMessage(message.chat, {
         image: { url: imagePath },
