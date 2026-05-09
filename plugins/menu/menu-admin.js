593239a88eceff8f3e498e7ba3b900e6f15fa0fc
rileydomina333 <indoxxabile666@gmail.com>
2026-05-09 12:08:00 +0200

    Update menu-admin.js

--- plugins/menu/menu-admin.js
+++ plugins/menu/menu-admin.js
@@ -9,7 +9,7 @@ const handler = async (message, { conn, usedPrefix, command }) => {
     const groupId = message.isGroup ? message.chat : null;
 
     const menuText = generateMenuText(usedPrefix, userId, groupId);
-    const imagePath = path.join(__dirname, '../../media/IMG-20260501-WA0549.jpg');
+    const imagePath = path.join(__dirname, '../../media/IMG-20260509-WA0069.jpg');
     const footerText = global.t('chooseMenu', userId, groupId);
     const mainMenuText = global.t('mainMenuButton', userId, groupId);
     const ownerMenuText = global.t('ownerMenuButton', userId, groupId);
